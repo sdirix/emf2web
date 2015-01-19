12bfb90106650da61e1e22b344a9adcdf5df1820
Stefan Dirix <sdirix@eclipsesource.com>
2015-01-19 18:31:37 +0100

    Externalize Strings

---
+++ org.eclipse.emf.ecp.emf2web/src/org/eclipse/emf/ecp/emf2web/wizard/pages/Messages.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Stefan Dirix - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.ecp.emf2web.wizard.pages;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Stefan Dirix
+ *
+ * @generated
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.emf.ecp.emf2web.wizard.pages.messages"; //$NON-NLS-1$
+	public static String EClassPage_ConstructorTitle;
+	public static String EClassPage_Description;
+	public static String EClassPage_Title;
+	public static String ModelPathsPage_AllFiles;
+	public static String ModelPathsPage_EnterProjectName;
+	public static String ModelPathsPage_SelectProject;
+	public static String ModelPathsPage_TemplateZipFile;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
