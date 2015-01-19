e82927ebf419fa25da6c107805609603b41a0b84
Stefan Dirix <sdirix@eclipsesource.com>
2015-01-19 18:09:07 +0100

    Fix all Checkstyle Warnings

--- org.eclipse.emf.ecp.emf2web/src/org/eclipse/emf/ecp/emf2web/export/NameHelper.java
+++ org.eclipse.emf.ecp.emf2web/src/org/eclipse/emf/ecp/emf2web/export/NameHelper.java
@@ -22,12 +22,21 @@ import org.eclipse.emf.ecp.view.model.common.edit.provider.CustomReflectiveItemP
 import org.eclipse.emf.edit.provider.AdapterFactoryItemDelegator;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 
+/**
+ * Helper class for determining the name of Ecore model elements and features as displayed by the
+ * {@link ItemPropertyDescriptor}s of EMF.
+ *
+ */
 public class NameHelper {
 
-	ComposedAdapterFactory composedAdapterFactory;
-	AdapterFactoryItemDelegator adapterFactoryItemDelegator;
+	private final ComposedAdapterFactory composedAdapterFactory;
+	private final AdapterFactoryItemDelegator adapterFactoryItemDelegator;
 
+	/**
+	 * Default Constructor.
+	 */
 	public NameHelper() {
 		composedAdapterFactory = new ComposedAdapterFactory(
 			new AdapterFactory[] {
@@ -39,7 +48,15 @@ public class NameHelper {
 			composedAdapterFactory);
 	}
 
-	private final IItemPropertyDescriptor getItemPropertyDescriptor(
+	/**
+	 * Returns the {@link ItemPropertyDescriptor} of the given {@link Setting}.
+	 *
+	 * @param setting
+	 *            The {@link Setting} for which the {@link ItemPropertyDescriptor} is to be determined.
+	 * @return
+	 *         The {@link ItemPropertyDescriptor} of the given {@link Setting}.
+	 */
+	private IItemPropertyDescriptor getItemPropertyDescriptor(
 		Setting setting) {
 		final IItemPropertyDescriptor descriptor = adapterFactoryItemDelegator
 			.getPropertyDescriptor(setting.getEObject(),
@@ -47,11 +64,33 @@ public class NameHelper {
 		return descriptor;
 	}
 
+	/**
+	 * Returns the name of the given {@link Setting} as displayed by the corresponding {@link ItemPropertyDescriptor}.
+	 *
+	 * @param setting
+	 *            The {@link Setting} for which the name is to be determined.
+	 * @return
+	 *         The name of the {@link Setting} as displayed by the corresponding {@link ItemPropertyDescriptor}. If no
+	 *         {@link ItemPropertyDescriptor} exists the unmodified name is returned.
+	 */
 	public String getDisplayName(Setting setting) {
 		final IItemPropertyDescriptor descriptor = getItemPropertyDescriptor(setting);
 		return descriptor.getDisplayName(setting.getEObject());
 	}
 
+	/**
+	 * Returns the name of the given {@link EStructuralFeature} as displayed by the corresponding
+	 * {@link ItemPropertyDescriptor}.
+	 *
+	 * @param eClass
+	 *            The {@link EClass} which contains the {@link EStructuralFeature}.
+	 * @param feat
+	 *            The {@link EStructuralFeature} for which the name is to be determined.
+	 * @return
+	 *         The name of the {@link EStructuralFeature} as displayed by the corresponding
+	 *         {@link ItemPropertyDescriptor}. If no {@link ItemPropertyDescriptor} exists the unmodified name is
+	 *         returned.
+	 */
 	public String getDisplayName(EClass eClass, EStructuralFeature feat) {
 		final EObject object = EcoreUtil.create(eClass);
 		final IItemPropertyDescriptor descriptor = adapterFactoryItemDelegator
