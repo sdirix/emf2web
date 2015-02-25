734fe11251bb70620d6c8978c09a383bf7969311
Stefan Dirix <sdirix@eclipsesource.com>
2015-02-25 15:23:09 +0100

    Backport some Javascript and CSS Changes

--- org.eclipse.emf.ecp.emf2web.examples/projects/org.eclipse.emf.ecp.emf2web.playapplication/public/js/directives.js
+++ org.eclipse.emf.ecp.emf2web.examples/projects/org.eclipse.emf.ecp.emf2web.playapplication/public/js/directives.js
@@ -1,22 +1,6 @@
 'use strict';
 
-var qbFormsDirectives = angular.module('myApp.directives', []);
-
-qbFormsDirectives.directive('collection', function() {
-    return {
-        restrict: "E",
-        replace: true,
-        scope: {
-            collection: '=',
-            bindings: '=',
-            openDate: '&',
-            validateNumber: "&",
-            validateInteger: "&"
-        },
-        template: '<element ng-repeat="element in collection" element="element"></element>'
-    };
-});
-
+var qbFormsDirectives = angular.module('qbForms.directives', []);
 
 qbFormsDirectives.directive('control', function() {
     return {
@@ -25,75 +9,30 @@ qbFormsDirectives.directive('control', function() {
         scope: {
             control: '=',
             bindings: '=',
-            openDate: '&',
-            validateNumber: "&",
-            validateInteger: "&"
+            topOpenDate: '=',
+            topValidateNumber: '=',
+            topValidateInteger: '='
         },
-        templateUrl: '/assets/templates/control.html'
+        templateUrl: 'templates/control.html'
     };
 });
 
-
-qbFormsDirectives.directive('element', function($compile) {
+qbFormsDirectives.directive('recelement', function(RecursionHelper) {
     return {
         restrict: "E",
         replace: true,
         scope: {
             element: '=',
             bindings: '=',
-            openDate: '&',
-            validateNumber: "&",
-            validateInteger: "&"
+            topOpenDate: '=',
+            topValidateNumber: '=',            
+            topValidateInteger: '='
         },
-        //template: '<div ng-switch on="element.type" class="element.class"></div>',
-        link: function(scope, element, attrs) {
-            var type = scope.element.type;
-
-            var toCompile;
-
-            scope.element.layoutclass = "col-sm-" + scope.element.size;
-
-            if (type === "Control") {
-                toCompile = '<control control="element.elements[0]" bindings="bindings" open-date="openDate(event, element)" validate-number="validateNumber(value, element)">';
-
-                // set the sizes for label and input
-                var size = scope.element.size;
-
-                //used when displaying labels next to the input
-                //var labelsize = Math.floor(size/3);
-                scope.element.elements[0].labelclass = ""; //"col-sm-" + labelsize;
-                scope.element.elements[0].inputclass = ""; //"col-sm-" + (size - labelsize);
-
-                scope.element.elements[0].wholesize = scope.element.layoutclass;
-
-            } else if (type === "HorizontalLayout") {
-            	//scope.element.layoutclass = scope.element.layoutclass + " form-inline";
-                toCompile = '<fieldset ng-class="element.layoutclass">' +
-                    '<div class="row">' +
-                    '<collection collection="element.elements" bindings="bindings" open-date="openDate(event, element)" validate-number="validateNumber(value, element)"></collection>' +
-                    '</div>' +
-                    '</fieldset>';
-            } else if (type === "VerticalLayout"){
-                 toCompile = '<fieldset ng-class="element.layoutclass">' +
-
-                    '<collection collection="element.elements" bindings="bindings" open-date="openDate(event, element)" validate-number="validateNumber(value, element)"></collection>' +
-
-                    '</fieldset>';           	
-            } else if (type === "Label"){
-                scope.element.layoutclass = scope.element.layoutclass + " qblabel";
-
-            	toCompile = '<div ng-class="element.layoutclass">' +
-	            	'{{element.elements[0].value}}' +
-	            	'</div>';
-            } else{
-            	toCompile = "<!-- COULD NOT DETERMINE TYPE OF ELEMENT -->";
-            }
-
-            $compile(toCompile)(scope, function(cloned, scope) {
-                element.replaceWith(cloned);
-            });
-
+        templateUrl: 'templates/element.html',
+        compile: function(element){
+            return RecursionHelper.compile(element);
         }
     };
 });
+
 
